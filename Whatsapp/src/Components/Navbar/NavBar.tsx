a9f2c161713d17ca0994a57babb911c38252d16f
Madhushree B <boylemadhushree@gmail.com>
2025-05-05 23:35:32 +0200

    navbar cards

--- Whatsapp/src/Components/Navbar/NavBar.tsx
+++ Whatsapp/src/Components/Navbar/NavBar.tsx
@@ -1,8 +1,11 @@
 import DownloadButton from "../Buttons/DownloadButton";
 import LoginButton from "../Buttons/LoginButton";
 import WhatsApp from "../../assets/whatsapp-green.svg";
+import React, { useState } from "react";
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex flex-row items-center justify-between my-4 px-10 pt-2 pb-5 bg-[rgba(252,245,235,.8)] ">
       <div className="logo w-[130px] h-[30px] ">
@@ -11,12 +14,34 @@ function NavBar() {
 
       <div className="contents">
         <div className="middle-contents flex flex-row gap-9">
-          <p>Features</p>
-          <p>Privacy</p>
-          <p>Help Center</p>
-          <p>Blog</p>
-          <p>For Business</p>
-          <p>Apps</p>
+          <div
+            className="relative"
+            onClick={() => setIsOpen(true)}
+            onMouseLeave={() => setIsOpen(false)}
+          >
+            <p className="cursor-pointer">Features</p>
+
+            {isOpen && (
+              <div className="absolute left-0 mt-2 w-40 bg-white rounded">
+                <ul className="flex flex-col">
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    Feature 1
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    Feature 2
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    Feature 3
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+          <p className="cursor-pointer">Privacy</p>
+          <p className="cursor-pointer">Help Center</p>
+          <p className="cursor-pointer">Blog</p>
+          <p className="cursor-pointer">For Business</p>
+          <p className="cursor-pointer">Apps</p>
         </div>
       </div>
 
